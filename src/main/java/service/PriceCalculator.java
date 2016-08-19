3214196f74d16109314f8faab4803023dc7ed9e2
Zoltan Toth <v-ztoth@expedia.com>
2016-08-19 11:01:57 +0200

    PriceCalculator initial implementation

---
+++ src/main/java/service/PriceCalculator.java
@@ -0,0 +1,9 @@
+package service;
+
+public class PriceCalculator
+{
+    public Long calculate()
+    {
+        return  10L;
+    }
+}
--- src/test/java/service/PriceCalculatorTest.java
+++ src/test/java/service/PriceCalculatorTest.java
@@ -1,5 +1,50 @@
 package service;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+@Test(groups = {"unit"})
 public class PriceCalculatorTest
 {
+    private PriceCalculator priceCalculator;
+    private Long actualPrice;
+    private Long exceptedPrice;
+
+    @BeforeMethod
+    public void setup()
+    {
+        priceCalculator = null;
+        actualPrice = null;
+        exceptedPrice = null;
+    }
+
+    @Test
+    public void testCalculate()
+    {
+        givenAPriceCalculator();
+        givenExceptedPrice();
+        whenCalculateCalled();
+        thenPriceCalculated();
+    }
+
+    private void givenAPriceCalculator()
+    {
+        priceCalculator = new PriceCalculator();
+    }
+
+    private void givenExceptedPrice()
+    {
+        exceptedPrice = 10L;
+    }
+
+    private void whenCalculateCalled()
+    {
+        actualPrice = priceCalculator.calculate();
+    }
+
+    private void thenPriceCalculated()
+    {
+        Assert.assertEquals(actualPrice, exceptedPrice, "Actual price should be" + exceptedPrice);
+    }
 }
